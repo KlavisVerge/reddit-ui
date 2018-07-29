f43953bb60495a47659e62ecc82aa9c36666e8c7
Nick Phillips <imprecatorr@gmail.com>
2018-07-28 22:54:11 -0500

    Give ability to search for one string while displaying another

--- reddit-ui.js
+++ reddit-ui.js
@@ -41,7 +41,7 @@ class RedditUi extends PolymerElement {
         }
       </style>
       <paper-spinner id="spinner" active=[[active]]></paper-spinner>
-      <paper-item><h3>[[game]] - Hot Reddit Posts</h3></paper-item>
+      <paper-item><h3>[[displayName]] - Hot Reddit Posts</h3></paper-item>
 
       <template is="dom-repeat" items="[[posts]]">
         <div class="wrapper">
@@ -59,6 +59,10 @@ class RedditUi extends PolymerElement {
       game: {
         type: String,
         value: 'Fortnite',
+      },
+      displayname: {
+        type: String,
+        value: 'Fortnite'
       },
       active: {
         type: Boolean,
